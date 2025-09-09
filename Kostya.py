acabb61d38ece9d56365f30ed845dc1b0fbbcef4
Kostyakiag <kostyafoothack@gmail.com>
2025-09-09 11:03:29 +0300

    Update Kostya.py

--- Kostya.py
+++ Kostya.py
@@ -1,1 +1,20 @@
 name="kostya"
+age=17
+km=12.4
+my_favorite_games=[FIFA25, GTA5]
+my_shortcomings=[]
+student=true
+schoolboy=false
+shopping=(bananas, meat, water)
+my_favorite_cars={AUDIr7, Aventador, PORSHE911}
+pets={"1";: "dog", "2"; "cat "}
+print (name, type (name) )
+print (age, type (age) )
+print (km, type (km) )
+print(my_favorite_games, type (my_favorite_games)
+print (my_shortcomings, type(my_shortcoming))
+print (student, type (student) )
+print (schoolboy, type (schoolboy))
+print (shopping, type (sopping))
+print (my_favorite_cars, type(my_favorite_cars))
+print(pets, type (pets) )
